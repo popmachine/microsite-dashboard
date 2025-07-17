c34141b8bbc8ef122f69b41e8deb70332df39b39
Mike Moen <mikemoen@Mac.attlocal.net>
2025-07-17 16:57:11 -0500

    Fix: Restore views folder and Breeze install

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\MicrositeController;
 use Illuminate\Support\Facades\Route;
 
-// Redirect unauthenticated users to login
 Route::get('/', function () {
-    return redirect()->route('login');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
@@ -17,12 +15,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    // Microsite routes
-    Route::get('/microsites', [MicrositeController::class, 'index'])->name('microsites.index');
-    Route::get('/microsites/create', [MicrositeController::class, 'create'])->name('microsites.create');
-    Route::post('/microsites', [MicrositeController::class, 'store'])->name('microsites.store');
-    Route::post('/microsites/{id}/launch', [MicrositeController::class, 'launch'])->name('microsites.launch');
 });
 
 require __DIR__.'/auth.php';
