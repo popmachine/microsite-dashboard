d2082c2d1ffec8dfe2260381d248afa7c1548ed0
Mike Moen <mikemoen@Mac.attlocal.net>
2025-07-19 15:40:34 -0500

    Remove Vite from guest layout

--- resources/views/components/guest-layout.blade.php
+++ resources/views/components/guest-layout.blade.php
@@ -12,7 +12,8 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700&display=swap" rel="stylesheet" />
 
     <!-- Styles and Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
+<script src="{{ asset('build/assets/app.js') }}" defer></script>
 
     <style>
         h1#page-title {
