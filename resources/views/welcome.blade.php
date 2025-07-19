b7d3530bfeedfec7608e50e74e246938e21bad2f
Mike Moen <mikemoen@Mac.attlocal.net>
2025-07-19 18:02:49 -0500

    Build Tailwind CSS without Vite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,15 @@
 // dummy
+<!-- resources/views/welcome.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <h1>It works 🎉</h1>
+    <p>This view does not use Vite.</p>
+</body>
+</html>
